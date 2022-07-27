30af1e23544df01d38f3524764600b98f4904d3b
Luca Ghislotti <ghislottiluca@gmail.com>
2022-07-27 12:27:53 +0200

    Added TC plots

--- plots.m
+++ plots.m
@@ -9,33 +9,43 @@ for i = 1:length(index_interpreter)
 end
 
 TCs = readtable('TC_all_TPs.csv');
-TC_labels = nan(16, 1);
 
-for i = 1:16
-    TC_labels(i) = "Var" + num2str(i);
+TC_labels1 = strings(16, 1);
+TC_labels2 = strings(16, 1);
+
+k1 = 1;
+for k2 = 1:3:46
+    TC_labels1(k1, 1) = strcat("Var", num2str(k2));
+    TC_labels2(k1, 1) = strcat("Var", num2str(k2+1)); % +1 per mean su X, +2 per slope su X
+    k1 = k1 + 1;
 end
 
 TCs_means = nan(16, 16);
 lgd = cell(16,1) ;
 
 for j = 1:16
-    f = figure('Visible', 'on');
+    f = figure('Visible', 'off');;
     colors = distinguishable_colors(16, 'w');
     hold on
     
     for i = 0:15
-        TCs_means(:, i+1) = TCs.(TC_labels(num2str(j)))(TCs.Var3==i);
+        TCs_means(:, i+1) = TCs.(TC_labels1(j))(TCs.(TC_labels2(j))==i);
         plot([0:15], TCs_means(:, i+1), 'Color', [colors(i+1, 1), colors(i+1, 2), colors(i+1, 3)]);
-        lgd{i+1} = strcat('Mean = ', " ", num2str(i));
+        lgd{i+1} = strcat('Slope = ', " ", num2str(i));
     end
     
-    legend(lgd, 'NumColumns', 2)
+    legend(lgd, 'Location', 'northwest', 'NumColumns', 2)
     box on
     grid on
-    title('\textbf{TP' + num2str(j) + '}')
-    xlabel('Slope')
+    title(['\textbf{TP', num2str(j), '}'])
+    xlabel('Mean')
     ylabel('TC')
     xticks([0:15])
     
-    exportgraphics(gcf,'output/slope_on_x/TP' + num2str(j) + '_TC_slope.pdf','ContentType','vector');
+    set(gca,'FontSize', 15)
+    f.Position = [200 160 1080  800];
+    exportgraphics(gcf,['output/TC/mean_on_x/TP', num2str(j), '_TC_mean.pdf'],'ContentType','vector');
 end
+
+fileID_out = fopen('output/TC/TC_mean_on_x.dat', 'w');
+fprintf(fileID_out ,' %f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n', TCs_means);
