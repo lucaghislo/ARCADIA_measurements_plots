99781afdfa14ceed04040df88e9312f49da77feb
Luca Ghislotti <ghislottiluca@gmail.com>
2022-12-27 12:43:19 +0100

    Added plot 7 optimal values for all TPs at every temperature

--- scripts/plot_7.py
+++ scripts/plot_7.py
@@ -50,6 +50,7 @@ TPs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 Vins = [1.08, 1.2, 1.32]
 colors = distinctipy.get_colors(len(TPs))
 markers = ["o", "s", "v", "^", "<", ">", "p", "*", "d", "D", "h", "H", 5, 6, 7, 8]
+volt_ref = 0.6
 
 R0 = 7
 R2 = 7
@@ -58,7 +59,11 @@ Vin = Vins[1]
 plt.clf()
 TP_index = 0
 for TP in TPs:
-    Volt_values_TP = np.zeros(shape=(len(temperatures_str), 1))
+    volt_values = np.zeros(shape=(len(temperatures_str), 1))
+    delta_values = np.zeros(shape=(len(temperatures_str), 1))
+    index_values = np.zeros(shape=(len(temperatures_str), 1))
+    mean_values = np.zeros(shape=(len(temperatures_str), 1))
+    slope_values = np.zeros(shape=(len(temperatures_str), 1))
     temp_index = 0
     for temp in temperatures_str:
         data_plot6 = pd.read_csv(
@@ -70,18 +75,85 @@ for TP in TPs:
 
         data_plot6_Vin = data_plot6[data_plot6["Vin"] == Vin]
         data_plot6_Vin_TP = data_plot6_Vin[data_plot6_Vin["TP"] == TP]
-        data_plot6_Vin_TP_R0 = data_plot6_Vin_TP[data_plot6_Vin_TP["SLOPE"] == R0]
-        data_plot6_Vin_TP_R0_R2 = data_plot6_Vin_TP_R0[
-            data_plot6_Vin_TP_R0["MEAN"] == R2
-        ]
-        Volt = data_plot6_Vin_TP_R0_R2["Volt"].mean()
 
-        Volt_values_TP[temp_index] = Volt
+        min_index = 0
+        min_delta = abs(volt_ref - data_plot6_Vin_TP.iloc[0]["Volt"])
+        min_Volt = data_plot6_Vin_TP.iloc[0]["Volt"]
+        min_mean = data_plot6_Vin_TP.iloc[0]["MEAN"]
+        min_slope = data_plot6_Vin_TP.iloc[0]["SLOPE"]
+        for index, row in data_plot6_Vin_TP.iterrows():
+            delta = abs(volt_ref - row["Volt"])
+            if delta < min_delta:
+                min_delta = delta
+                min_index = index
+                min_Volt = row["Volt"]
+                min_mean = row["MEAN"]
+                min_slope = row["SLOPE"]
+
+        volt_values[temp_index] = min_Volt
+        delta_values[temp_index] = min_delta
+        index_values[temp_index] = min_index
+        mean_values[temp_index] = min_mean
+        slope_values[temp_index] = min_slope
+
         temp_index = temp_index + 1
+
+        # Save estimated values
+        filename = "TP_" + str(TP) + "_values.dat"
+        txt_filepath_drive = os.path.join(output_folder_drive, filename)
+        txt_filepath_github = os.path.join(output_folder_github, filename)
+
+        with open(
+            txt_filepath_drive,
+            "w",
+        ) as fp:
+            counter = 1
+            fp.write("Temp\tVout\tDelta\tIndex\tMean\tSlope\n")
+            for i in range(0, len(volt_values) - 1):
+                fp.write(
+                    str(temperatures_int[i])
+                    + "\t"
+                    + str(float(volt_values[i]))
+                    + "\t"
+                    + str(float(volt_values[i]))
+                    + "\t"
+                    + str(int(index_values[i]))
+                    + "\t"
+                    + str(int(mean_values[i]))
+                    + "\t"
+                    + str(int(slope_values[i]))
+                    + "\n"
+                )
+                counter = counter + 1
+
+        with open(
+            txt_filepath_github,
+            "w",
+        ) as fp:
+            counter = 1
+            fp.write("Temp\tVout\tDelta\tIndex\tMean\tSlope\n")
+            for i in range(0, len(volt_values) - 1):
+                fp.write(
+                    str(temperatures_int[i])
+                    + "\t"
+                    + str(float(volt_values[i]))
+                    + "\t"
+                    + str(float(volt_values[i]))
+                    + "\t"
+                    + str(int(index_values[i]))
+                    + "\t"
+                    + str(int(mean_values[i]))
+                    + "\t"
+                    + str(int(slope_values[i]))
+                    + "\n"
+                )
+                counter = counter + 1
+
+    print(volt_values)
 
     plt.plot(
         temperatures_int,
-        Volt_values_TP,
+        volt_values,
         linewidth=1,
         marker=markers[TP_index],
         markerfacecolor=colors[TP_index],
@@ -95,54 +167,11 @@ plt.xlabel(r"Temperature [$^{\circ}$C]")
 plt.ylabel(r"$V_{OUT}$ [V]")
 plt.title(r"\boldmath$V_{OUT}$ \textbf{vs} \textbf{Temperature for every bandgap}")
 plt.xticks(temperatures_int)
+min_y = volt_ref - 0.005
+max_y = volt_ref + 0.005
+plt.ylim(min_y, max_y)
+plt.yticks(np.arange(min_y, max_y, 0.001))
 plt.legend(title=r"\textbf{Bandgap}", loc="center left", bbox_to_anchor=(1, 0.5))
 plt.grid()
 
 print_plot(output_folder_drive, output_folder_github, "plot_7.pdf")
-
-
-# Select nearest Vout to 600 mV
-for TP in TPs:
-    Volt_values_TP = np.zeros(shape=(len(temperatures_str), 1))
-    temp_index = 0
-    for temp in temperatures_str:
-        data_plot6 = pd.read_csv(
-            os.path.join(
-                TP_temp_slope_mean_vout,
-                "Results_TP" + str(TP) + "_REG_" + str(temp) + ".csv",
-            )
-        )
-
-        data_plot6_Vin = data_plot6[data_plot6["Vin"] == Vin]
-        data_plot6_Vin_TP = data_plot6_Vin[data_plot6_Vin["TP"] == TP]
-        data_plot6_Vin_TP_R0 = data_plot6_Vin_TP[data_plot6_Vin_TP["SLOPE"] == R0]
-        data_plot6_Vin_TP_R0_R2 = data_plot6_Vin_TP_R0[
-            data_plot6_Vin_TP_R0["MEAN"] == R2
-        ]
-        Volt = data_plot6_Vin_TP_R0_R2["Volt"].mean()
-
-        Volt_values_TP[temp_index] = Volt
-        temp_index = temp_index + 1
-
-    plt.plot(
-        temperatures_int,
-        Volt_values_TP,
-        linewidth=1,
-        marker=markers[TP_index],
-        markerfacecolor=colors[TP_index],
-        color=colors[TP_index],
-        label=str(TP_index + 1),
-    )
-
-    TP_index = TP_index + 1
-
-plt.xlabel(r"Temperature [$^{\circ}$C]")
-plt.ylabel(r"$V_{OUT}$ [V]")
-plt.title(
-    r"\boldmath$V_{OUT}$ \textbf{vs} \textbf{Temperature for every bandgap (optimal R0)}"
-)
-plt.xticks(temperatures_int)
-plt.legend(title=r"\textbf{Bandgap}", loc="center left", bbox_to_anchor=(1, 0.5))
-plt.grid()
-
-print_plot(output_folder_drive, output_folder_github, "plot_6_optimized_R0.pdf")
